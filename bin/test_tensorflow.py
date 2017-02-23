4e8361a65b4a1ea1d85ef7da336e43f96ea4764b
Daniel Paschall <dpaschall@gmail.com>
2017-02-22 22:49:29 -0800

    Fixed Mandelbrotset.py, and updated README in bin

--- bin/test_tensorflow.py
+++ bin/test_tensorflow.py
@@ -1,10 +1,19 @@
 # This script tests if TensorFlow is setup properly
 # Be sure to use python version 3.5.x
 # install tensor flow using pip3
+
+# ONLY DISPLAY ERRORS NOT WARNINGS!
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 #
 
+# the beginning of every tensorflow script:
 import tensorflow as tf
+#
 
+# Hello World:
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
 print(sess.run(hello))
+
+#end of file
