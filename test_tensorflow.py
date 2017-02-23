8101a421951535dbf6e6f934ae90362ce664fae3
Daniel Paschall <Daniel Paschall>
2017-02-22 20:56:34 -0800

    Updated test_tensorflow.py with comments in title

--- test_tensorflow.py
+++ test_tensorflow.py
@@ -1,3 +1,8 @@
+# This script tests if TensorFlow is setup properly
+# Be sure to use python version 3.5.x
+# install tensor flow using pip3
+#
+
 import tensorflow as tf
 
 hello = tf.constant('Hello, TensorFlow!')
